9927f9f937e6204241d94828d2e3917e2fe348aa
pastpap <stefan.papin@gmail.com>
2025-03-19 15:20:42 +0100

    code formatting

--- src/mancala_game_service.ads
+++ src/mancala_game_service.ads
@@ -1,17 +1,19 @@
 -- File: mancala_game_service.ads
 with Mancala_Game; use Mancala_Game;
+
 package Mancala_Game_Service is
 
    type Mancala_Game_State is record
-      Board       : Mancala_Game.Board_Array (0 .. Mancala_Game.Board_Size-1);
-      Turn_Player : Natural;
-      Is_Finished : Boolean;
-      Winning_Player : Integer;
+      Board                :
+        Mancala_Game.Board_Array (0 .. Mancala_Game.Board_Size - 1);
+      Turn_Player          : Natural;
+      Is_Finished          : Boolean;
+      Winning_Player       : Integer;
       Winning_Player_Score : Integer;
-      Message     : String(1 .. 256);
+      Message              : String (1 .. 256);
    end record;
 
-   procedure Play_Hand(Position : Natural; State : in out Mancala_Game_State);
+   procedure Play_Hand (Position : Natural; State : in out Mancala_Game_State);
    function Reset_Game return Mancala_Game_State;
 
 end Mancala_Game_Service;
